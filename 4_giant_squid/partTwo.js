0b176c84d9c7a7bd7e8d8e616b49332a6ebeb78d
Ramy <ramydoki@gmail.com>
2021-12-04 20:42:53 +0100

    Day 4

---
+++ 4_giant_squid/partTwo.js
@@ -0,0 +1,84 @@
+const readline = require('readline');
+const fs = require('fs');
+
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./input.txt')
+});
+  
+let nbLines = 0
+let arrayOfNumbers = undefined
+
+let countRow = 0
+let countBoard= 0
+let tabOfBoards = [[]]
+
+lineReader.on('line', function (line) {
+    nbLines++
+        if(nbLines<2)
+            arrayOfNumbers = line.split(',')
+        else{
+            if(!(line == "")){
+                if (countRow==5){
+                    countRow=0
+                    countBoard++
+                    tabOfBoards[countBoard] = new Array()
+                }
+                    tabOfBoards[countBoard].push(line.split(' ').filter((elt)=>elt != ""))
+                    countRow++
+            }
+         }
+});
+
+const checker = (tab) =>{
+    for(let i = 0; i<5; i++){
+        if(tab[i].filter( e=> e=='-1').length == 5){
+            return true
+        }
+    }
+    
+    let inversed = inverseTab(tab) 
+
+    for(let i = 0; i<5; i++){
+        if(inversed[i].filter( e=> e=='-1').length == 5){
+           return true
+        }
+    }
+
+    return false
+}
+
+const inverseTab = (tab) => {
+    let resp = [];
+
+    tab.map((e, x ) => {
+        e.map((e, y)=>{
+            if(!resp[y])
+            resp[y] = new Array()
+            if(!resp[y][x])
+            resp[y][x] = new Array()
+            resp[y][x] = e
+        })
+    })
+
+    return resp
+} 
+
+lineReader.on('close', ()=>{
+    let score = 0
+    arrayOfNumbers.forEach(num => {
+        tabOfBoards.forEach((board, index) => {
+            if(board){
+                for(let i = 0; i<5; i++){
+                    board[i] = board[i].map(e => e == num ? '-1' : e)
+                }
+                if(checker(board)){
+                    if(tabOfBoards.filter(e => e).length == 1){
+                        board.flat().map(e => e == '-1' ? '' : score+=parseInt(e))
+                        console.log(score*num)
+                    }
+                    tabOfBoards[index] = undefined
+                }
+            }
+            })
+        })
+})
